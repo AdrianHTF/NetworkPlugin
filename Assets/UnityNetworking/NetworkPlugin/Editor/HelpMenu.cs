47a96c619ee2938c51686dd146d52fea865f3cb3
AdrianHTF <a.fien@gmx.de>
2019-01-28 16:11:49 +0100

    Update HelpMenu.cs
    updated HelpText

--- Assets/UnityNetworking/NetworkPlugin/Editor/HelpMenu.cs
+++ Assets/UnityNetworking/NetworkPlugin/Editor/HelpMenu.cs
@@ -8,7 +8,7 @@ public class NewBehaviourScript : EditorWindow
     [MenuItem("Network/Help", false, 51)]
     private static void NewMenuItemHelp()
     {
-        Application.OpenURL("file://C:/Users/trbea/OneDrive/BachelorArbeit/Unity/VRDemo/Assets/NetworkPlugin/new.html");
+        Application.OpenURL("https://github.com/AdrianHTF/NetworkPlugin");
     }
 }
 
